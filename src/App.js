3f7230714344e3427b0487bbc48dc82383d01a0e
NicoCobian <nicotdf123@hotmail.com>
2022-04-27 19:49:29 +0200

    desafio Nicolas Cobian numero 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import  Navbar  from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Esta es mi primera app en react.js</h1>
+      
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar () {
+  return (
+    <header className='header'>
+      <nav>
+        <li>inicio</li>
+        <li>producto</li>
+        <li>contacto</li>
+      </nav>
+
+
+    </header>
+    
+  )
+}
+
